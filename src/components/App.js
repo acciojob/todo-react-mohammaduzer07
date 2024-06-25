067841f655b755a8907475f3d2cbab71aee8292f
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-06-25 16:25:26 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,41 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [addTodo, setTodo] = useState([]);
+
+  let handleToDo = () => {
+    if (input !== "") {
+      let todo = input;
+      setInput("")
+      setTodo(prev => [todo, ...prev])
+    }
+  }
+  let handleDelete = (index) => {
+    let updateTodo = addTodo.filter((todo, i) => i !== index)
+    setTodo(updateTodo);
+    console.log(updateTodo);
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <div className="main">
+
+      <h3>To-Do-List</h3>
+
+      <div>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={handleToDo}>Add Todo</button>
+      </div>
+
+      <ul>
+        {addTodo && addTodo.length>0 && addTodo.map((todo, index) => {
+          return <li className="todos" key={index}> <p>{todo}</p> <button onClick={() => handleDelete(index)}>Delete</button></li>
+        })}
+      </ul>
+
+    </div >
   )
 }
 
